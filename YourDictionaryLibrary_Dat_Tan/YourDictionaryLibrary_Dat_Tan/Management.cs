dc1020f8f37c7bcbe9953bfb75c7b4c71d46d8e3
ThanhTanNguyen2000 <thanhtanpm2000@gmail.com>
2019-12-06 09:38:37 +0700

    Hoàn Thành Manage

--- YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Management.cs
+++ YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Management.cs
@@ -36,47 +36,9 @@ namespace YourDictionaryLibrary_Dat_Tan
             cbType.SelectedItem = null;
             rtbMeaning.Text = null;
         }
-        private void BtnEdit_Click(object sender, EventArgs e)
-        {
-            if (grdW.SelectedRows.Count == 1)
-            {
-                manage_Enable_Disable(true);
-                var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
-                txtEnglishW.Text = Word.English_Word;
-                cbType.SelectedItem = Word.Word_type;
-                rtbMeaning.Text = Word.Meaning;
-                this.btnSave.Click += BtnSave_Click_Edit;
-                this.btnCancel.Click += BtnCancel_Click;
-            }
-        }
-
-        private void BtnSave_Click_Edit(object sender, EventArgs e)
-        {
-            var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
-            var EngW = txtEnglishW.Text;
-            var Type = (string)cbType.SelectedItem;
-            var Meaning = rtbMeaning.Text;
-            this.Business.EditWord(Word.ID, EngW, Type, Meaning);
-            MessageBox.Show("Edit Compelete", "Nofication", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            manage_Enable_Disable(false);
-            Null();
-            load_Words();
-        }
-
-        private void BtnDelete_Click(object sender, EventArgs e)
-        {
-            if (grdW.SelectedRows.Count == 1)
-            {
-                var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
-                this.Business.DeleteWord(Word.ID);
-                MessageBox.Show("Delete Compelete", "Nofication", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                load_Words();
-            }
-        }
-
         private void manage_Enable_Disable(bool IsEnable)
         {
-            if(IsEnable == true)
+            if (IsEnable == true)
             {
                 this.btnSave.Enabled = true;
                 this.btnCancel.Enabled = true;
@@ -92,19 +54,13 @@ namespace YourDictionaryLibrary_Dat_Tan
                 this.cbType.Enabled = false;
                 this.rtbMeaning.Enabled = false;
             }
-        } 
+        }
         private void BtnAdd_Click(object sender, EventArgs e)
         {
             manage_Enable_Disable(true);
             this.btnSave.Click += BtnSave_Click_Add;
-            this.btnCancel.Click += BtnCancel_Click;           
+            this.btnCancel.Click += BtnCancel_Click;
         }
-
-        private void BtnCancel_Click(object sender, EventArgs e)
-        {
-            manage_Enable_Disable(false);
-        }
-
         private void BtnSave_Click_Add(object sender, EventArgs e)
         {
             var EngW = txtEnglishW.Text;
@@ -117,18 +73,53 @@ namespace YourDictionaryLibrary_Dat_Tan
             Null();
             load_Words();
         }
-
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            manage_Enable_Disable(false);
+        }
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            if (grdW.SelectedRows.Count == 1)
+            {
+                manage_Enable_Disable(true);
+                var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
+                txtEnglishW.Text = Word.English_Word;
+                cbType.SelectedItem = Word.Word_type;
+                rtbMeaning.Text = Word.Meaning;
+                this.btnSave.Click += BtnSave_Click_Edit;
+                this.btnCancel.Click += BtnCancel_Click;
+            }
+        }
+        private void BtnSave_Click_Edit(object sender, EventArgs e)
+        {
+            var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
+            var EngW = txtEnglishW.Text;
+            var Type = (string)cbType.SelectedItem;
+            var Meaning = rtbMeaning.Text;
+            this.Business.EditWord(Word.ID, EngW, Type, Meaning);
+            MessageBox.Show("Edit Compelete", "Nofication", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            manage_Enable_Disable(false);
+            Null();
+            load_Words();
+        }
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (grdW.SelectedRows.Count == 1)
+            {
+                var Word = (Word)grdW.SelectedRows[0].DataBoundItem;
+                this.Business.DeleteWord(Word.ID);
+                MessageBox.Show("Delete Compelete", "Nofication", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                load_Words();
+            }
+        }
         private void BtnReload_Click(object sender, EventArgs e)
         {
             load_Words();
         }
-
         private void Management_Load(object sender, EventArgs e)
         {
-            var LoadWord = this.Business.GetWords();
-            grdW.DataSource = LoadWord;
+            load_Words();
         }
-
         private void BtnExit_Click(object sender, EventArgs e)
         {
             this.Close();
