9041f2f61a29ea47dbb25eace9e52b86248c95ed
ThanhTanNguyen2000 <thanhtanpm2000@gmail.com>
2019-12-06 11:35:22 +0700

    Hoàn thành chức năng web

--- YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Management.Designer.cs
+++ YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Management.Designer.cs
@@ -263,6 +263,7 @@
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnReload);
             this.Controls.Add(this.btnEdit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Management";
