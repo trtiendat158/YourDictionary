64e8b0c3c761bbc8b81482ba703cdd1cee6a803c
Tien Dat Tr <bryce.gazelle2000@gmail.com>
2019-12-17 10:41:00 +0700

    DONE

--- YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Main_Interface.Designer.cs
+++ YourDictionaryLibrary_Dat_Tan/YourDictionaryLibrary_Dat_Tan/Main_Interface.Designer.cs
@@ -49,7 +49,7 @@
             this.lblTitle.Location = new System.Drawing.Point(203, 41);
             this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(409, 31);
+            this.lblTitle.Size = new System.Drawing.Size(515, 38);
             this.lblTitle.TabIndex = 31;
             this.lblTitle.Text = "YOUR DICTIONARY LIBRARY";
             // 
@@ -147,6 +147,8 @@
             // 
             // btnTest
             // 
+            this.btnTest.Image = ((System.Drawing.Image)(resources.GetObject("btnTest.Image")));
+            this.btnTest.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnTest.Location = new System.Drawing.Point(236, 348);
             this.btnTest.Name = "btnTest";
             this.btnTest.Size = new System.Drawing.Size(183, 42);
@@ -156,7 +158,7 @@
             // 
             // Main_Interface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
